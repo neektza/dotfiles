1bf23fa0bc554dc0c45ffe8b7b399db49d269db7
Nikica Jokic <nikica.jokic@infinum.hr>
2011-07-25 16:04:42 +0200

    case insensitive grep

--- bash_settings.sh
+++ bash_settings.sh
@@ -2,7 +2,9 @@
 
 # Some commons
 export GIT_EDITOR=vim
+export RAILS_ENV=development_emobile
 alias ack='ack -ri'
+alias grep='grep -ri'
 
 # Load RVM
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
