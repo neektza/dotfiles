9e7c0476db9cb8deb208802e8cd28e7821f3b908
Nikica Jokic <neektza@gmail.com>
2026-05-30 00:42:19 +0200

    Auto-switch remote theme based on macOS appearance via LC_APPEARANCE

--- zshrc.d/pwrbook.zsh
+++ zshrc.d/pwrbook.zsh
@@ -1,1 +1,8 @@
 # pwrbook-specific config
+
+# forward macOS appearance to remote shells via LC_ (picked up by SSH SendEnv LC_*)
+if [[ "$(defaults read -g AppleInterfaceStyle 2>/dev/null)" == "Dark" ]]; then
+  export LC_APPEARANCE=dark
+else
+  export LC_APPEARANCE=light
+fi
